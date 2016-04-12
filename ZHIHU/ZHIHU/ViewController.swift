65263e56cfff831ef812c6c2a28d6ca06b97cfad
wzh <wang@wodewi-fi.local>
2016-04-12 18:25:14 +0800

    初始化

---
+++ ZHIHU/ZHIHU/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ZHIHU
+//
+//  Created by wzh on 16/4/12.
+//  Copyright © 2016年 谈超. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
