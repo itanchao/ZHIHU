9d0ce9cbd4a5c875097003e5d9e57296db0497c5
tanchao <wang@wodewi-fi.local>
2016-05-06 17:41:14 +0800

    js

--- ZHIHU/ZHIHU/EXTENSION(扩展)/tools.js
+++ ZHIHU/ZHIHU/EXTENSION(扩展)/tools.js
@@ -1,9 +1,15 @@
 
+/// 获取图片路径
 var getimageSrc = function (prefixStr){
     var objs = document.getElementsByTagName("img");
     var imageList = [];
     for(var i=0;i<objs.length;i++)
     {
+        /**
+         *  @author 谈超, 16-05-06 17:05:11
+         *
+         *  遍历每一个图片元素，添加点击事件
+         */
         objs[i].onclick=function()
         {
             alert(this.src);
@@ -11,6 +17,11 @@ var getimageSrc = function (prefixStr){
         };
         imageList.push(objs[i].src);
     };
+    /**
+     *  @author 谈超, 16-05-06 17:05:19
+     *
+     *  第一次加载获取所有图片路径
+     */
     document.location= "imagelist:"+imageList;
     return imageList;
 //    return objs.length;
